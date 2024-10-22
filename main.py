29f4c41876b53329e4c0d75c7613782c083dd8fd
jassyjazz <starletsnow@gmail.com>
2024-10-22 19:01:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -338,7 +338,7 @@ if not check_password():
     st.write("Please enter the correct password above to access the content.")
 else:
     if page == "Home":
-        col1, col2 = st.columns([1, 4])
+        col1, col2 = st.columns([1, 5])
         
         with col1:
             st.image("https://raw.githubusercontent.com/jassyjazz/bootcampai/main/hdb_logo.png", width=100)
